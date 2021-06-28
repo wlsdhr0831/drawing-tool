f4173b2c532f5ca343aff536e4b8fb36a7881406
성진옥 <wlsdhr0831@naver.com>
2021-06-28 16:09:25 +0900

    [feat] add konva and set components

---
+++ src/components/Menu.js
@@ -0,0 +1,19 @@
+function Menu () {
+    return (
+        <div>
+            <button>뒤로 돌리기</button>
+            <button>앞으로 돌리기</button>
+            <button>직선</button>
+            <button>곡선</button>
+            <button>원</button>
+            <button>직사각형</button>
+            <button>다각형</button>
+            <button>선 두께</button>
+            <input type="number"/>
+            <button>색상</button>
+            <button>지우개</button>
+        </div>
+    )
+}
+
+export default Menu;
---
+++ src/components/Workspace.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Stage } from 'react-konva';
+
+function Workspace () {
+    return (
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+
+        </Stage>
+    )
+}
+
+export default Workspace;
